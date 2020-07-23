4fe0a1296f1f18a932889508b0bf01f15cdfe0aa
Thiago Demas <thiago.demas7@gmail.com>
2020-07-23 20:16:59 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
+
+
+export default function Main() {
+  return <MapView style={{flex: 1}} />
+}
+
