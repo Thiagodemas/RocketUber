5d47d706096e326f09c62d4ab9206f629b79a0ec
Thiago Demas <thiago.demas7@gmail.com>
2020-07-24 14:13:32 -0300

    add images and travel time

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,17 +1,26 @@
 import React, {useEffect, useState, useRef} from 'react';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
 import {View} from 'react-native';
 import Search from "../../components/Search";
 import Directions from "../../components/Directions";
+import markerImage from '../../assets/marker.png';
+import {
+  Back,
+  LocationBox,
+  LocationText,
+  LocationTimeBox,
+  LocationTimeText,
+  LocationTimeTextSmall
+} from "./styles";
+
 
 function Main() {
 
   const [currentRegion, setCurrentRegion] = useState(null);
   const [currentDestination, setCurrentDestination] = useState(null);
-  const [currentDuration, setCurrentDuration] = useState(null);
-  const [currentResult, setCurrentResult] = useState(null);
-    const mapView = useRef(null);
+  const [currentDuration, setCurrentDuration ] = useState("");
+  const mapView = useRef(null);
 
 
   useEffect(() => {
@@ -42,7 +51,8 @@ function Main() {
   if (!currentRegion) {
     return null;
   }
-  async function handleLocationSelected(data, { geometry }) {
+
+  async function handleLocationSelected(data, {geometry}) {
     const {
       location: {
         lat: latitude,
@@ -58,37 +68,55 @@ function Main() {
   }
 
 
-
   return <View style={{flex: 1}}>
-
     <MapView
       style={{flex: 1}}
       initialRegion={currentRegion}
       showsUserLocation={true}
       loadingEnabled={true}
-      ref={mapView} >
+      ref={mapView}>
 
       {currentDestination && (
-        <Directions
-          origin={currentRegion}
-          destination={currentDestination}
-          onReady={result => {
-            setCurrentDuration({ currentDuration: Math.floor(result.currentDuration) });
-            setCurrentResult({currentResult: result});
-            mapView.current.fitToCoordinates(result.coordinates, {
-              edgePadding: {
-                right: 50,
-                left: 50,
-                top: 50,
-                bottom: 350
-              }
-            });
-          }}
-        />
+        <>
+          <Directions
+            origin={currentRegion}
+            destination={currentDestination}
+            onReady={result => {
+              setCurrentDuration( Math.floor(result.duration));
+              console.log("currentDuration", currentDuration );
+              mapView.current.fitToCoordinates(result.coordinates, {
+                edgePadding: {
+                  right: 80,
+                  left: 80,
+                  top: 80,
+                  bottom: 350
+                }
+              });
+            }}
+          />
+          <Marker
+            coordinate={currentDestination}
+
+            image={markerImage}
+          >
+            <LocationBox>
+              <LocationText>{currentDestination.title}</LocationText>
+            </LocationBox>
+          </Marker>
+          <Marker coordinate={currentRegion} >
+            <LocationBox>
+              <LocationTimeBox>
+                <LocationTimeText>{currentDuration}</LocationTimeText>
+                <LocationTimeTextSmall>MIN</LocationTimeTextSmall>
+              </LocationTimeBox>
+              <LocationText>Felipe Cortez, 867</LocationText>
+            </LocationBox>
+          </Marker>
+        </>
       )}
 
     </MapView>
-    <Search onLocationSelected={handleLocationSelected} />
+    <Search onLocationSelected={handleLocationSelected}/>
   </View>
 }
 
