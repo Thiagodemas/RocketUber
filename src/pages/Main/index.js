8dc5849cbf18c79b20a89b42a7eb8feb7f933e6b
Thiago Demas <thiago.demas7@gmail.com>
2020-07-23 21:06:02 -0300

    add map and geolocation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {View} from 'react-native';
 import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 
-export default function Main() {
-  return <MapView style={{flex: 1}} />
+function Main() {
+
+  const [ currentRegion, setCurrentRegion ] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+          maximumAge: 1000
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.0143,
+          longitudeDelta: 0.0134,
+        })
+      }
+
+    }
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion){
+    return null;
+  }
+
+  return <MapView
+    style={{flex: 1}}
+    initialRegion={currentRegion}
+    showsUserLocation={true}
+    loadingEnabled={true}
+  />
 }
+
+export default Main
 
